47ecfbf9161c44b2f95e19eb3b0d6f317f99eb2d
Lennard Wulf <145865459+l3nnardwlg@users.noreply.github.com>
2026-08-09 23:36:06 +0200

    bump version to 1.9.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "me.bounser"
-version = "1.9.7"
+version = "1.9.8"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_25
@@ -21,7 +21,7 @@ repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://oss.sonatype.org/content/groups/public/")
-    maven("https://maven.respark.dev/releases")
+    maven("https://repo.respark.dev/releases")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
     maven("https://m2.dv8tion.net/releases")
     maven("https://nexus.scarsz.me/content/groups/public/")
